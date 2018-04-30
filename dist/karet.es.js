a9a398d129c4bf1502ed8df98eb9155ed70414b5
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-04-30 11:54:15 +0300

    3.4.0

--- dist/karet.es.js
+++ dist/karet.es.js
@@ -1,14 +1,10 @@
-import { isArray, inherit, object0, isString, dissocPartialU, assocPartialU } from 'infestines';
+import { inherit, object0, isString, dissocPartialU, assocPartialU } from 'infestines';
+import { when, toFunction, flatten, ifElse, identity, values, branchOr, elems, choose, forEach, modify, select } from 'partial.lenses';
 import { Component, Fragment, createElement, forwardRef } from 'react';
 export { Children, Fragment, createContext, forwardRef } from 'react';
 import { Property } from 'kefir';
 
 //
-
-var STYLE = 'style';
-var DANGEROUSLY = 'dangerouslySetInnerHTML';
-
-var CHILDREN = 'children';
 
 var LIFT = 'karet-lift';
 
@@ -25,94 +21,19 @@ function valueOf(p) {
 
 //
 
-function renderChildren(children) {
-  var newChildren = children;
-  for (var i = 0, n = children.length; i < n; ++i) {
-    var v = children[i];
-    var w = isProperty(v) ? valueOf(v) : isArray(v) ? renderChildren(v) : v;
-    if (v !== w) {
-      if (newChildren === children) newChildren = children.slice(0);
-      newChildren[i] = w;
-    }
-  }
-  return newChildren;
-}
-
-function renderObject(object) {
-  var newObject = null;
-  for (var i in object) {
-    var v = object[i];
-    if (isProperty(v)) {
-      if (!newObject) {
-        newObject = {};
-        for (var j in object) {
-          if (j === i) break;
-          newObject[j] = object[j];
-        }
-      }
-      newObject[i] = valueOf(v);
-    } else if (newObject) {
-      newObject[i] = v;
-    }
-  }
-  return newObject || object;
-}
-
-function renderProps(props) {
-  var newProps = null;
-  for (var i in props) {
-    var v = props[i];
-    var w = isProperty(v) ? valueOf(v) : CHILDREN === i ? isArray(v) ? renderChildren(v) : v : STYLE === i || DANGEROUSLY === i ? renderObject(v) : v;
-    if (v !== w) {
-      if (!newProps) {
-        newProps = {};
-        for (var j in props) {
-          if (j === i) break;
-          newProps[j] = props[j];
-        }
-      }
-      newProps[i] = w;
-    } else if (newProps) {
-      newProps[i] = w;
-    }
-  }
-  return newProps || props;
-}
+var inProperty = /*#__PURE__*/when(isProperty);
+var inChildren = /*#__PURE__*/toFunction([flatten, inProperty]);
+var inValues = /*#__PURE__*/ifElse(isProperty, identity, [values, inProperty]);
+var inProps = /*#__PURE__*/branchOr(inProperty, {
+  children: inChildren,
+  style: inValues,
+  dangerouslySetInnerHTML: inValues
+});
+var inArgs = /*#__PURE__*/toFunction([elems, /*#__PURE__*/choose(function (_, i) {
+  return 1 < i ? inChildren : 1 === i ? inProps : inProperty;
+})]);
 
 //
-
-function forEachInChildren(i, children, extra, fn) {
-  for (var n = children.length; i < n; ++i) {
-    var c = children[i];
-    if (isProperty(c)) fn(extra, c);else if (isArray(c)) forEachInChildren(0, c, extra, fn);
-  }
-}
-
-function forEachInProps(props, extra, fn) {
-  for (var i in props) {
-    var v = props[i];
-    if (isProperty(v)) {
-      fn(extra, v);
-    } else if (CHILDREN === i) {
-      if (isArray(v)) forEachInChildren(0, v, extra, fn);
-    } else if (STYLE === i || DANGEROUSLY === i) {
-      for (var j in v) {
-        var vj = v[j];
-        if (isProperty(vj)) fn(extra, vj);
-      }
-    }
-  }
-}
-
-//
-
-function offAny(handler, property) {
-  property.offAny(handler);
-}
-
-function onAny(handler, property) {
-  property.onAny(handler);
-}
 
 function doSubscribe(self, _ref) {
   var args = _ref.args;
@@ -128,24 +49,18 @@ function doSubscribe(self, _ref) {
     }
   };
 
-  forEachInProps(args[1], handler, onAny);
-  forEachInChildren(2, args, handler, onAny);
+  forEach(function (p) {
+    return p.onAny(handler);
+  }, inArgs, args);
 }
 
 function doUnsubscribe(_ref2, _ref3) {
   var h = _ref2.h;
   var args = _ref3.args;
 
-  forEachInChildren(2, args, h, offAny);
-  forEachInProps(args[1], h, offAny);
-}
-
-function decObs(p2n, property) {
-  p2n.set(property, (p2n.get(property) || 0) - 1);
-}
-
-function incObs(p2n, property) {
-  p2n.set(property, (p2n.get(property) || 0) + 1);
+  forEach(function (p) {
+    return p.offAny(h);
+  }, inArgs, args);
 }
 
 function updateObs(delta, property, p2n) {
@@ -166,19 +81,16 @@ var FromClass = /*#__PURE__*/inherit(function FromClass(props) {
   componentDidMount: function componentDidMount() {
     doSubscribe(this, this.props);
   },
-  componentDidUpdate: function componentDidUpdate(_ref4) {
-    var before = _ref4.args;
-    var after = this.props.args;
-
-
+  componentDidUpdate: function componentDidUpdate(prevProps) {
     var p2n = new Map();
     p2n.h = this.h;
 
-    forEachInProps(before[1], p2n, decObs);
-    forEachInChildren(2, before, p2n, decObs);
-
-    forEachInProps(after[1], p2n, incObs);
-    forEachInChildren(2, after, p2n, incObs);
+    forEach(function (p) {
+      return p2n.set(p, (p2n.get(p) || 0) - 1);
+    }, inArgs, prevProps.args);
+    forEach(function (p) {
+      return p2n.set(p, (p2n.get(p) || 0) + 1);
+    }, inArgs, this.props.args);
 
     p2n.forEach(updateObs);
   },
@@ -187,18 +99,9 @@ var FromClass = /*#__PURE__*/inherit(function FromClass(props) {
   },
   render: function render() {
     if (this.h) {
-      var args = this.props.args;
-
-      var n = args.length;
-      var newArgs = Array(n);
-      var type = newArgs[0] = args[0];
-      newArgs[1] = renderProps(args[1]);
-      for (var i = 2; i < n; ++i) {
-        var v = args[i];
-        newArgs[i] = isProperty(v) ? valueOf(v) : isArray(v) ? renderChildren(v) : v;
-      }
-      if (type === Fragment && n < 4 && null == newArgs[2]) return null;
-      return createElement.apply(null, newArgs);
+      var args = modify(inArgs, valueOf, this.props.args);
+      if (args[0] === Fragment && args.length < 3) return null;
+      return createElement.apply(null, args);
     } else {
       return null;
     }
@@ -213,41 +116,14 @@ var FromClass = /*#__PURE__*/inherit(function FromClass(props) {
 
 //
 
-function hasPropertiesInChildren(i, children) {
-  for (var n = children.length; i < n; ++i) {
-    var c = children[i];
-    if (isProperty(c) || isArray(c) && hasPropertiesInChildren(0, c)) return true;
-  }
-  return false;
-}
-
-function hasPropertiesInProps(props) {
-  for (var i in props) {
-    var v = props[i];
-    if (isProperty(v)) {
-      return true;
-    } else if (CHILDREN === i) {
-      if (isArray(v) && hasPropertiesInChildren(0, v)) return true;
-    } else if (STYLE === i || DANGEROUSLY === i) {
-      for (var j in v) {
-        if (isProperty(v[j])) return true;
-      }
-    }
-  }
-  return false;
-}
-
-//
-
-function considerLifting(args) {
-  var props = args[1];
-  return hasPropertiesInProps(props) || hasPropertiesInChildren(2, args) ? createElement(FromClass, { args: args, key: props.key }) : createElement.apply(null, args);
-}
+var considerLifting = function considerLifting(args) {
+  return select(inArgs, args) ? createElement(FromClass, { args: args, key: args[1].key }) : createElement.apply(null, args);
+};
 
 function createElement$1(type, props, _child) {
   props = props || object0;
   var lift = props[LIFT];
-  if (lift || isString(type) || Fragment === type) {
+  if (lift || isString(type) || Fragment === type || isProperty(type)) {
     var n = arguments.length;
     var args = Array(n);
     args[0] = type;
